e846006f95b7d9f5c5d6eafde2d5ac4fe4fc87c5
Carmen Li <cmnli95@gmail.com>
2023-10-12 21:08:28 -0400

    carmen pushing phase 0 the dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Carmen is the champion'
+document.body.append(newHeader);
